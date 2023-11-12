06dde93fa7378847951f5c84cfdc1c52d1f122da
adastratechnologiesllc <147508238+adastratechnologiesllc@users.noreply.github.com>
2023-11-12 02:33:29 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Initialize the mobile side navigation menu
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
